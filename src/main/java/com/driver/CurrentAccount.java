f608ac77e73cf8327e7b35ee4b58a0979a840aed
Suraj Singh <mesinghzr@gmail.com>
2023-12-29 20:23:24 +0530

    m

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -12,39 +12,39 @@ public class CurrentAccount extends BankAccount{
     }
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-
+        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
         super(name, 5000);
         this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
 
-        Map<Character, Integer> hm = new HashMap<>();
-        for (char c : tradeLicenseId.toCharArray()) {
-            hm.put(c, hm.getOrDefault(c, 0) + 1);
-        }
-
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        for (char key : hm.keySet()) {
-
-            maxHeap.offer(new int[]{key, hm.get(key)});
-        }
-
-        StringBuilder res = new StringBuilder();
-        int[] prev = new int[]{'*', 0};
-
-        while (!maxHeap.isEmpty()) {
-            int[] current = maxHeap.poll();
-            res.append((char) current[0]);
-            if (prev[1] > 0) {
-                maxHeap.offer(prev);
+            Map<Character, Integer> hm = new HashMap<>();
+            for (char c : tradeLicenseId.toCharArray()) {
+                hm.put(c, hm.getOrDefault(c, 0) + 1);
             }
 
-            current[1]--;
-            prev = current;
-        }
-        if (res.toString() != tradeLicenseId) {
-            throw new Exception("Valid License can not be generated");
-        }
+            PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+            for (char key : hm.keySet()) {
+
+                maxHeap.offer(new int[]{key, hm.get(key)});
+            }
+
+            StringBuilder res = new StringBuilder();
+            int[] prev = new int[]{'*', 0};
+
+            while (!maxHeap.isEmpty()) {
+                int[] current = maxHeap.poll();
+                res.append((char) current[0]);
+                if (prev[1] > 0) {
+                    maxHeap.offer(prev);
+                }
+
+                current[1]--;
+                prev = current;
+            }
+            if (res.toString() != tradeLicenseId) {
+                throw new Exception("Valid License can not be generated");
+            }
     }
 }
