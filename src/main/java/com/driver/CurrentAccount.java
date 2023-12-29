45d03d0a5baaf0c3b7a1054050c4b35661e80fd3
Suraj Singh <mesinghzr@gmail.com>
2023-12-29 20:18:34 +0530

    m

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,38 +1,50 @@
 package com.driver;
 
-public class CurrentAccount extends BankAccount {
-    private String tradeLicenseId;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
-    private static final double DEFAULT_MIN_BALANCE = 5000;
+public class CurrentAccount extends BankAccount{
+    String tradeLicenseId; //consists of Uppercase English characters only
 
-    public CurrentAccount(String name, String tradeLicenseId) {
-        super(name, DEFAULT_MIN_BALANCE);
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
+
+        super(name, 5000);
         this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
-        char[] licenseChars = tradeLicenseId.toCharArray();
-        for (int i = 0; i < licenseChars.length - 1; i++) {
-            if (licenseChars[i] == licenseChars[i + 1]) {
-                // Rearrange the characters to create a valid license Id
-                rearrangeLicenseId();
-                return;
-            }
-        }
-        // License Id is already valid
-    }
 
-    private void rearrangeLicenseId() throws Exception {
-        char[] licenseChars = tradeLicenseId.toCharArray();
-        for (int i = 0; i < licenseChars.length - 1; i++) {
-            if (licenseChars[i] == licenseChars[i + 1]) {
-                // Swap adjacent characters
-                char temp = licenseChars[i];
-                licenseChars[i] = licenseChars[i + 1];
-                licenseChars[i + 1] = temp;
-            }
+        Map<Character, Integer> hm = new HashMap<>();
+        for (char c : tradeLicenseId.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
         }
-        tradeLicenseId = new String(licenseChars);
-        validateLicenseId(); // Recursively validate the rearranged license Id
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        for (char key : hm.keySet()) {
+
+            maxHeap.offer(new int[]{key, hm.get(key)});
+        }
+
+        StringBuilder res = new StringBuilder();
+        int[] prev = new int[]{'*', 0};
+
+        while (!maxHeap.isEmpty()) {
+            int[] current = maxHeap.poll();
+            res.append((char) current[0]);
+            if (prev[1] > 0) {
+                maxHeap.offer(prev);
+            }
+
+            current[1]--;
+            prev = current;
+        }
+        if (res.toString() != tradeLicenseId) {
+            throw new Exception("Valid License can not be generated");
+        }
     }
 }
